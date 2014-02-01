a99108d64b0e8796f2651148a6046f14fa9d1da3
Nemesis <reza.piri@gennovacap.com>
2014-01-31 22:34:55 -0600

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,31 @@
+class HomeController < ApplicationController
+	
+
+	def index
+	end	
+
+	def login
+	end
+	
+	def signup
+	end
+
+	def bitcoincharts
+	end
+
+	def buybitcoin
+	end
+
+	def contact
+	end
+
+	def sellbitcoin
+	end
+
+	def security
+	end
+
+		
+
+	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitcoinagents::Application.config.secret_key_base = '74b04e44849f8e6a70a93016fd8be74a071877bd09cd4fcbf190fa63d0207b7046973ef540469d5f9873b877a4364e134509f03ca7282921ae4e0538cdbde326'
