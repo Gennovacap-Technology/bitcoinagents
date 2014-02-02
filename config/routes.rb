9fd3b40700ae9eaa2041fb52c3a577a03d849232
Nemesis <reza.piri@gennovacap.com>
2014-02-02 13:54:14 -0600

    added views for payment_methods buyorder_bitcoin_invoice sellorder_bitcoin_invoice

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Bitcoinagents::Application.routes.draw do
 
   # User Login and Dashboard
   get "dashboard"   =>    'home#dashboard',   :as =>  :dashboard
+  get "profile"   =>    'home#profile',   :as =>  :profile
+  get "buyorder_bitcoin_invoice"   =>    'home#buyorder_bitcoin_invoice',   :as =>  :buyorder_bitcoin_invoice
+  get "sellorder_bitcoin_invoice"   =>    'home#sellorder_bitcoin_invoice',   :as =>  :sellorder_bitcoin_invoice
   get "dashboard2"   =>    'home#dashboard2',   :as =>  :dashboard2
+  get "payment_methods"   =>    'home#payment_methods',   :as =>  :payment_methods
   get "transaction_login_history"   =>    'home#transaction_login_history',   :as =>  :transaction_login_history
   get "bitcoincharts"   =>    'home#bitcoincharts',  :as => :bitcoincharts
   get "orderbook"   =>    'home#orderbook',  :as => :orderbook
